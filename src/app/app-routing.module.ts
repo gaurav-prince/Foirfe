4741b022359002e8d68b473851f59b332baff69e
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2020-06-02 01:53:18 +0530

    Components created, routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ContactComponent } from './contact/contact.component';
+import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'what', component: WhatWeDoComponent },
+  { path: 'who', component: WhoWeAreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ContactComponent } from './contact/contact.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { bootstrap } from 'bootstrap/dist/js/bootstrap.min.js';
+import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResourcesComponent,
+    ContactComponent,
+    WhatWeDoComponent,
+    WhoWeAreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

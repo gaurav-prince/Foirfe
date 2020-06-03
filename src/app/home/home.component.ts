a4a5866a00b359dc1f1fab9d9fade131adf2ed7f
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2020-06-03 17:08:22 +0530

    Home page css changed and animation text added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { sign } from 'crypto';
+import Typed from 'typed.js';
+import { TimeoutError } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +13,37 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  aTitle = ["Simple", "Robust", "Sustainable"];
+  sTitle: string = "";
+
   ngOnInit(): void {
+    this.sTitle = this.aTitle[0];
+    var typed = new Typed('#typedText', {
+      strings: ["Simple", "Robust", "Sustainable"],
+      typeSpeed: 80,
+      loop: true,
+      backSpeed: 30,
+      backDelay: 1000,
+      startDelay: 500,
+      showCursor: false
+    });
+    // this.changeTitle();
+  }
+
+  changeTitle() {
+    const that = this;
+    let aIndex, sText, sIndex;
+    setInterval(function () {
+      sIndex = 0;
+      aIndex = that.aTitle.findIndex((element) => element === that.sTitle);
+      sText = that.aTitle[(1 + aIndex) % that.aTitle.length];
+      that.sTitle = "";
+      setInterval(function () {
+        that.sTitle = that.sTitle + sText.charAt(sIndex);
+        sIndex++;
+      }, 300)
+      console.log(that.sTitle);
+    }, 5000)
   }
 
 }

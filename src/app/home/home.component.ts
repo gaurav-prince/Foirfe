6bbce583cd6f1197fd9438126907973e27f324bc
Gaurav Prince <gkumar475@gmail.com>
2020-08-26 20:07:40 +0530

    carousel slide changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -46,31 +46,5 @@ export class HomeComponent implements OnInit {
     }, 5000)
   }
 
-  carouselOptions = {
-    margin: 25,
-    nav: true,
-    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true
-      },
-      600: {
-        items: 1,
-        nav: true
-      },
-      1000: {
-        items: 2,
-        nav: true,
-        loop: false
-      },
-      1500: {
-        items: 3,
-        nav: true,
-        loop: false
-      }
-    }
-  }
 
 }
